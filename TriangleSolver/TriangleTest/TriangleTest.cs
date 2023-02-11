ccdc44287fed1ce3ba71fbf106c87983b10ee843
LT-OILEHIN\Al <freddy.j.lozano@gmail.com>
2023-02-11 13:08:51 -0500

    Added validation test for isosceles. Checks that side 1 = 2, and 1 + 2 are greater than 3. Assertion successful

--- TriangleSolver/TriangleTest/TriangleTest.cs
+++ TriangleSolver/TriangleTest/TriangleTest.cs
@@ -27,13 +27,61 @@ namespace TriangleTest
             int secondSide = 2;
             int thirdSide = 2;
 
-            string outputMessage = "";
+            string actualMessage = "";
 
-            outputMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             string expectedOutput = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
 
-            Assert.AreEqual(expectedOutput, outputMessage);
+            Assert.AreEqual(expectedOutput, actualMessage);
         }
+
+        // Three (3) tests for a valid isosceles triangle
+        /// <summary>
+        /// Tests for Isosceles. Checks if properly validates that sides 1 and 2 are the same length, but not side 3.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_IsoscelesSide1and2_Valid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 3;
+            int secondSide = 3;
+            int thirdSide = 5;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
+        // Three (3) tests for a valid isosceles triangle
+        /// <summary>
+        /// Tests for first invalid triangle check. Checks if having sum of sides 1 and 2 
+        /// less than side 3 triggers proper output error.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_TriangleSidesLessThanThird_Invalid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 2;
+            int secondSide = 2;
+            int thirdSide = 4;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "Based on the values entered, the triangle is INVALID";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
     }
 }
