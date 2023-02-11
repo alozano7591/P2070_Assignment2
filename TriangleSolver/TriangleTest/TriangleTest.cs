33d727ad2ff9122bed5679ddf65d5f675d8e0eb8
LT-OILEHIN\Al <freddy.j.lozano@gmail.com>
2023-02-11 14:12:44 -0500

    Added unit tests for 3 cases of 0 length line cases, and 3 for other possible error outcomes

--- TriangleSolver/TriangleTest/TriangleTest.cs
+++ TriangleSolver/TriangleTest/TriangleTest.cs
@@ -107,7 +107,7 @@ namespace TriangleTest
 
         //  Five (5) tests for a valid scalene triangle
         /// <summary>
-        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// Tests for Scalene. 1st.
         /// </summary>
         [Test]
         public void TriangleSolver_ScaleneOne_Valid()
@@ -130,7 +130,7 @@ namespace TriangleTest
 
         //  Five (5) tests for a valid scalene triangle
         /// <summary>
-        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// Tests for Scalene. 2nd
         /// </summary>
         [Test]
         public void TriangleSolver_ScaleneTwo_Valid()
@@ -153,7 +153,7 @@ namespace TriangleTest
 
         //  Five (5) tests for a valid scalene triangle
         /// <summary>
-        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// Tests for Scalene. 3rd
         /// </summary>
         [Test]
         public void TriangleSolver_ScaleneThree_Valid()
@@ -176,7 +176,7 @@ namespace TriangleTest
 
         //  Five (5) tests for a valid scalene triangle
         /// <summary>
-        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// Tests for Scalene. 4th
         /// </summary>
         [Test]
         public void TriangleSolver_ScaleneFour_Valid()
@@ -199,7 +199,7 @@ namespace TriangleTest
 
         //  Five (5) tests for a valid scalene triangle
         /// <summary>
-        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// Tests for Scalene. 5th
         /// </summary>
         [Test]
         public void TriangleSolver_ScaleneFive_Valid()
@@ -220,6 +220,75 @@ namespace TriangleTest
             Assert.AreEqual(expectedOutput, actualMessage);
         }
 
+        //  Three (3) tests for verifying a zero length for one or more sides
+        /// <summary>
+        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_ZeroLengthSideOne_InValid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 0;
+            int secondSide = 6;
+            int thirdSide = 5;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "At least one side of your triangle has a zero length and is thus invalid";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
+        //  Three (3) tests for verifying a zero length for one or more sides
+        /// <summary>
+        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_ZeroLengthSideTwo_InValid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 6;
+            int secondSide = 0;
+            int thirdSide = 5;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "At least one side of your triangle has a zero length and is thus invalid";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
+        //  Three (3) tests for verifying a zero length for one or more sides
+        /// <summary>
+        /// Tests for Isosceles. Checks if properly validates that sides 3 and 2 are the same length, but not side 3.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_ZeroLengthSideThree_InValid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 6;
+            int secondSide = 5;
+            int thirdSide = 0;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "At least one side of your triangle has a zero length and is thus invalid";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
         // Three (3) tests for a valid isosceles triangle
         /// <summary>
         /// Tests for first invalid triangle check. Checks if having sum of sides 1 and 2 
@@ -233,6 +302,54 @@ namespace TriangleTest
 
             int firstSide = 2;
             int secondSide = 2;
+            int thirdSide = 4;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "Based on the values entered, the triangle is INVALID";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
+        // Three (3) tests for a valid isosceles triangle
+        /// <summary>
+        /// Tests for first invalid triangle check. Checks if having sum of sides 1 and 3 
+        /// less than side 2 triggers proper output error.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_TriangleSidesLessThanSecond_Invalid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 3;
+            int secondSide = 8;
+            int thirdSide = 4;
+
+            string actualMessage = "";
+
+            actualMessage = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            string expectedOutput = "Based on the values entered, the triangle is INVALID";
+
+            Assert.AreEqual(expectedOutput, actualMessage);
+        }
+
+        // Three (3) tests for a valid isosceles triangle
+        /// <summary>
+        /// Tests for first invalid triangle check. Checks if having sum of sides 3 and 2 
+        /// less than side 1 triggers proper output error.
+        /// </summary>
+        [Test]
+        public void TriangleSolver_TriangleSidesLessThanFirst_Invalid()
+        {
+
+            Triangle triangle = new Triangle();
+
+            int firstSide = 2;
+            int secondSide = 7;
             int thirdSide = 4;
 
             string actualMessage = "";
